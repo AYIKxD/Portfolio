b1f997d28ec1ce2351f03408d885ec2dad75aa1a
AYIKxD <ayushmanbhowmik74@gmail.com>
2026-01-29 20:57:42 +0530

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://public.wodniack.dev',
+  site: 'https://AYIKxD.github.io',
+  base: '/Portfolio',
 
   scopedStyleStrategy: 'class',
 
